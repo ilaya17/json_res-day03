11a3707c6beecb2f0ee675caf3040879cf21ded4
ilaya17 <ilayabharathi178@gmail.com>
2023-01-21 16:50:21 +0530

    add for.js

---
+++ for.js
@@ -0,0 +1,78 @@
+//1st question - LOOPS
+var res = [{
+    "fName" : "Ilayabharathi",
+    "lName" : "Sakthivel",
+    "age" : "21",
+    "education" : "UG graduated",
+    "degree" : "Bachelor of computer application",
+    "address" : "2, Palaniyappa colony,Annathanapatty,salem-636002",
+    "experience" : "fresher",
+    "languageKnown" : "tam,eng"
+}]
+//for in loop
+// for(key in res){
+//     console.log(key,res[key]);
+// }
+
+//output
+
+/*
+address: "2, Palaniyappa colony,Annathanapatty,salem-636002"
+age: "21"
+degree: "Bachelor of computer application"
+education: "UG graduated"
+experience: "fresher"
+fName: "Ilayabharathi"
+lName: "Sakthivel"
+languageKnown: "tam,eng"
+*/
+
+//for loop
+
+// for(let i=0; i<res.length; i++){
+//     console.log(res[i]);
+// }
+
+//output
+
+/*
+{fName: 'Ilayabharathi', lName: 'Sakthivel', age: '21', education: 'UG graduated', degree: 'Bachelor of computer application', …}
+*/
+
+//for of loop 
+
+// for(out of res){
+//     console.log([out]);
+// }
+
+//output
+
+/*
+
+{fName: 'Ilayabharathi', lName: 'Sakthivel', age: '21', education: 'UG graduated', degree: 'Bachelor of computer application', …}
+     address
+: 
+"2, Palaniyappa colony,Annathanapatty,salem-636002"
+age
+: 
+"21"
+degree
+: 
+"Bachelor of computer application"
+education
+: 
+"UG graduated"
+experience
+: 
+"fresher"
+fName
+: 
+"Ilayabharathi"
+lName
+: 
+"Sakthivel"
+languageKnown
+: 
+"tam,eng"
+
+*/
